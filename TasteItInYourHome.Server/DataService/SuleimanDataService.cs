10d1d021fbccc6282d7697a13d876f9886b20848
SuleimanKh97 <suleiman97kh@gmail.com>
2025-04-19 00:32:22 +0300

    addcontactus

--- TasteItInYourHome.Server/DataService/SuleimanDataService.cs
+++ TasteItInYourHome.Server/DataService/SuleimanDataService.cs
@@ -1,4 +1,5 @@
-﻿using TasteItInYourHome.Server.IDataService;
+﻿using TasteItInYourHome.Server.DTOs;
+using TasteItInYourHome.Server.IDataService;
 using TasteItInYourHome.Server.Models;
 
 namespace TasteItInYourHome.Server.DataService
@@ -14,5 +15,22 @@ namespace TasteItInYourHome.Server.DataService
         {
             return _db.Services.ToList();
         }
+
+        public bool newContact(ContactUsRequest contact)
+        {
+            if (contact == null) 
+            {
+                return false;
+            }
+            var contactForm = new ContactU();
+            contactForm.FullName = contact.FullName;
+            contactForm.Email = contact.Email;
+            contactForm.Message = contact.Message;
+            contactForm.SubmittedAt = DateTime.Now;
+            _db.ContactUs.Add(contactForm);
+            _db.SaveChanges();
+            return true;
+        }
+
     }
 }
